9160488a23feda877ff45a4565eaeb9f32210d9f
Lahiru Prasad <104630433+Lahiru-LK@users.noreply.github.com>
2025-11-18 21:52:03 +0530

    Add listings page styles and car details section
    Introduced a new CSS file for listings page styles and updated the listings Blade view to include a car details section, image slider, tabbed content, and a price range sidebar. These changes enhance the UI and layout for individual car listings.

--- resources/views/public-site/listings.blade.php
+++ resources/views/public-site/listings.blade.php
@@ -11,6 +11,7 @@
 <link rel="stylesheet" href="{{ asset('NewAssts/css/sidebar.css') }}">
 <link rel="stylesheet" href="{{ asset('NewAssts/css/color.css') }}">
 <link rel="stylesheet" href="{{ asset('NewAssts/css/about.css') }}">
+<link rel="stylesheet" href="{{ asset('NewAssts/css/listings.css') }}">
 <!-- CDN & plugin CSS (also available in global header) -->
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
@@ -98,6 +99,84 @@
 
 @section('content')
 
+    <div class="block-title">
+        <div class="block-title__inner section-bg section-bg_second">
+            <div class="bg-inner">
+                <h1 class="ui-title-page">car details</h1>
+                <div class="decor-1 center-block"></div>
+                <ol class="breadcrumb">
+                    <li><a href="{{ route('home') }}">HOME</a></li>
+                    <li class="active">car details</li>
+                </ol>
+            </div>
+            <!-- end bg-inner -->
+        </div>
+        <!-- end block-title__inner -->
+    </div>
+    <!-- end block-title -->
 
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8">
+                <main class="main-content">
+                    <article class="car-details">
+                        <div class="car-details__wrap-title clearfix">
+                            <h2 class="car-details__title">Chevrolet Impala</h2>
+                            <div class="car-details__wrap-price">
+                                <span class="car-details__price"><span class="car-details__price-inner">Rs. 27,92000</span></span>
+                            </div>
+                        </div>
+
+                        <div id="slider-product" class="flexslider slider-product">
+                            <ul class="slides">
+                                <li>
+                                    <a href="{{ asset('NewAssts/media/slider_product/large/1.jpg') }}">
+                                        <img class="img-responsive" src="{{ asset('NewAssts/media/slider_product/large/1.jpg') }}" height="430" width="770" alt="Foto" />
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                        <div id="carousel-product" class="flexslider carousel-product">
+                            <ul class="slides">
+                                <li><img src="{{ asset('NewAssts/media/slider_product/small/1.jpg') }}" height="75" width="95" alt="foto" /></li>
+                            </ul>
+                        </div>
+
+                        <div class="wrap-nav-tabs">
+                            <ul class="nav nav-tabs">
+                                <li class="active"><a href="#tab1" data-toggle="tab">vehicle DESCRIPTION</a></li>
+                                <li><a href="#tab2" data-toggle="tab">features</a></li>
+                                <li><a href="#tab3" data-toggle="tab">inquiry</a></li>
+                            </ul>
+                        </div>
+
+                        <div class="tab-content">
+                            <div class="tab-pane active" id="tab1">
+                                <h3 class="ui-title-inner">Lorem ipsum dolor sit amet</h3>
+                                <div class="decor-1"></div>
+                                <p>Perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</p>
+                            </div>
+                        </div>
+                    </article>
+                </main>
+            </div>
+
+            <div class="col-md-4">
+                <aside class="sidebar">
+                    <div class="price-section">
+                        <div class="price-header">Price Range</div>
+                        <div class="price-value" id="priceRangeDisplay">Rs. 6,900 - Rs. 801,700</div>
+                        <div class="slider-container">
+                            <div id="sliderTrack"></div>
+                            <div id="sliderRange"></div>
+                            <input type="range" min="6900" max="801700" step="1000" value="6900" id="priceMin">
+                            <input type="range" min="6900" max="801700" step="1000" value="801700" id="priceMax">
+                        </div>
+                        <a href="{{ route('listings') }}" class="btn-search btn btn-primary">Search Inventory</a>
+                    </div>
+                </aside>
+            </div>
+        </div>
+    </div>
 
 @endsection
